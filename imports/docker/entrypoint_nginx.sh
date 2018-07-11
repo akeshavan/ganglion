07fc372935c662dd2b18d3aa3952849de44df85d
Shotgunosine <adenosine@gmail.com>
2018-07-11 17:16:40 -0400

    Make work on HPC

--- imports/docker/entrypoint_nginx.sh
+++ imports/docker/entrypoint_nginx.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-cd /home/mindcontrol/mindcontrol
-nohup meteor --settings mc_nginx_settings.json --port 2998 &
+cd ~
+if [ ! -x .meteor ]; then 
+	echo "Copying meteor files into singularity_home"
+	rsync -ach /home/mindcontrol/mindcontrol .
+	rsync -ach /home/mindcontrol/.meteor .
+	rsync -ach  /home/mindcontrol/.cordova .
+	ln -s /home/mindcontrol/mindcontrol/.meteor/local ~/mindcontrol/.meteor/local/
+fi
+cd ~/mindcontrol
+nohup meteor --settings /mc_settings/mc_nginx_settings.json --port 2998 &
 nginx -g "daemon off;"
